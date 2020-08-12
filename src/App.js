a7526d878c08e7c1319e27f1937417073253cb67
Pau Bonilla <paulo111995@gmail.com>
2020-08-12 16:19:09 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 function App() {
   return (
     <div>
-
+      <div className="wrapper">
+        <div className="nav">
+          <div className="left-colum"></div>
+          <div className="main-section"></div>
+          <div className="right-column"></div>
+        </div>
+      </div>
     </div>
   );
 }
