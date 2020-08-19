b2f504a11ddd041b154b0213262fe2ea00ebf749
Pau Bonilla <paulo111995@gmail.com>
2020-08-19 00:25:00 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export const HeaderStyle = styled.div`
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  background: #EBECF1;
+`
+
+export default function Header() {
+  return (
+    <HeaderStyle>
+      Header
+    </HeaderStyle>
+  )
+}
--- src/styled/index.js
+++ src/styled/index.js
@@ -1,16 +1,10 @@
 import styled from 'styled-components'
 
 export const AppContainer = styled.div`
-  width: 100vw;
-  height: 100vh;
-  display: flex;
-  `
+  width: 100%;
+`
 
 export const AppInner = styled.div`
-  background: #4e4e50;
-  width: 100%;
-  max-width: 600px;
-  margin: 0 auto;
   display: flex;
-  flex-direction: row;
-`
+  flex-direction: column;
+` 
