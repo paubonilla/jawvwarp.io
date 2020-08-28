9c27fc8c665a216b5aba60b2d357e9aac8229592
Pau Bonilla <paulo111995@gmail.com>
2020-08-27 22:33:04 -0700

    testing deploy if Failed

--- src/components/pages/AboutPage/About.js
+++ src/components/pages/AboutPage/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AboutSection from './AboutSection';
-import { aboutObjThree } from './data';
+import { aboutObjThree } from './Data';
 import Pricing from '../../Pricing'
 
 function About() {
--- src/components/pages/HomePage/Home.js
+++ src/components/pages/HomePage/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import HomeSection from '../../HomeSection'
-import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from '../HomePage/data'
+import { homeObjOne, homeObjTwo, homeObjThree, homeObjFour } from './Data'
 
 export default function Home() {
   return (
