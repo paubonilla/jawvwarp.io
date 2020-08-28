64657fbd5956969ee899e7aeb4ee8a082b79e565
Pau Bonilla <paulo111995@gmail.com>
2020-08-27 17:20:33 -0700

    modified components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { AppContainer, AppInner } from './styled'
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+// import { AppContainer, AppInner } from './styled'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Nav from './components/pages/Nav'
 import './App.scss';
 
 function App() {
   return (
-    <div>
-
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/" />
+      </Switch>
+    </Router>
   );
 }
 
