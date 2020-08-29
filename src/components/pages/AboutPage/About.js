34dfe91886d73a6505b5bad80399a8186a0de44f
Pau Bonilla <paulo111995@gmail.com>
2020-08-28 20:39:42 -0700

    initial commit

--- src/components/pages/AboutPage/About.js
+++ src/components/pages/AboutPage/About.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import AboutSection from './AboutSection';
-import { aboutObjOne } from './Data';
+import { aboutObjOne } from './data';
 // import Pricing from '../../Pricing'
 
 function About() {
