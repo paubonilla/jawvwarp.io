e8afe5994c31040a0aba49b10f032de9155892eb
Pau Bonilla <paulo111995@gmail.com>
2020-08-28 19:41:29 -0700

    fix People/.data

--- src/components/pages/People/People.js
+++ src/components/pages/People/People.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PeopleSection from './PeopleSection';
-import { peopleObjOne } from './Data';
+import { peopleObjOne } from './data';
 
 function People() {
   return (
